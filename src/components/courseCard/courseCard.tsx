384a248a060d349a77cd8421a89ecbae7c039053
natalielogi <logilogi93@yandex.ru>
2025-08-13 16:41:30 +0300

    главная страница

--- src/components/courseCard/courseCard.tsx
+++ src/components/courseCard/courseCard.tsx
@@ -1,41 +1,44 @@
 import { Course } from '@/sharedTypes/types';
 import Image from 'next/image';
+import styles from './courseCard.module.css';
 
 export default function CourseCard({ image, title, days, dailyMinutes, difficulty }: Course) {
   return (
-    <div className="card">
-      <Image className="card__img" src={image} alt={title} width={360} height={325} />
-      <div className="card__description">
-        <h2 className="card__title">{title}</h2>
-        <div className="card__timeDuration">
-          <Image
-            className="card__duration-img"
-            src="/card/Calendar.svg"
-            alt="calendar"
-            width={18}
-            height={18}
-          />
-          <p className="card__duration-p">{days} дней</p>
-        </div>
-        <div className="card__dailyDuration">
-          <Image
-            className="card__dailyDuration-img"
-            src="/card/Time.svg"
-            alt="time"
-            width={18}
-            height={18}
-          />
-          <p className="card__dailyDuration-p">{dailyMinutes}</p>
-        </div>
-        <div className="card__difficulty">
-          <Image
-            className="card__difficulty-img"
-            src="/card/difficulty.svg"
-            alt="difficulty"
-            width={18}
-            height={18}
-          />
-          <p className="card__difficulty-p">{difficulty}</p>
+    <div className={styles.card}>
+      <Image className={styles.card__img} src={image} alt={title} width={360} height={325} />
+      <div className={styles.card__description}>
+        <h2 className={styles.card__title}>{title}</h2>
+        <div className={styles.card__block}>
+          <div className={styles.card__timeDuration}>
+            <Image
+              className={styles.card__duration_img}
+              src="/card/Calendar.svg"
+              alt="calendar"
+              width={18}
+              height={18}
+            />
+            <p className={styles.card__duration_p}>{days} дней</p>
+          </div>
+          <div className={styles.card__dailyDuration}>
+            <Image
+              className={styles.card__dailyDuration_img}
+              src="/card/Time.svg"
+              alt="time"
+              width={18}
+              height={18}
+            />
+            <p className={styles.card__dailyDuration_p}>{dailyMinutes}</p>
+          </div>
+          <div className={styles.card__difficulty}>
+            <Image
+              className={styles.card__difficulty_img}
+              src="/card/difficulty.svg"
+              alt="difficulty"
+              width={18}
+              height={18}
+            />
+            <p className={styles.card__difficulty_p}>{difficulty}</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/courseCard/coursesGrid.tsx
+++ src/components/courseCard/coursesGrid.tsx
@@ -1,14 +1,26 @@
+'use client';
+
 import { mockCourses } from '@/lib/mocks/courses';
 import CourseCard from './courseCard';
+import styles from './courseCard.module.css';
 
 export default function Coursesgrid() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
   return (
     <section className="container-1440">
-      <div className="coutses">
+      <div className={styles.courses}>
         {mockCourses.map((course) => (
           <CourseCard key={course.id} {...course} />
         ))}
       </div>
+      <button className={`btn ${styles.back__btn}`} onClick={scrollToTop}>
+        Наверх ↑
+      </button>
     </section>
   );
 }
